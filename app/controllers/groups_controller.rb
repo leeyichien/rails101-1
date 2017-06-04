8aa8b43799be296a6a60cfc0ff799030c909a698
leeyichien <tdem.tdem@me.com>
2017-06-04 14:42:10 +0800

    install devise

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user! , only: [:new]
+ 
   def index
       @groups = Group.all
   end
@@ -29,7 +31,7 @@ class GroupsController < ApplicationController
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
-      render :edit 
+      render :edit
     end
   end
 
